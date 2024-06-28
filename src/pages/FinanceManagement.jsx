549155d434b36036e1a8d6331e571a41db93e2f9
bschall20 <bschall2016@gmail.com>
2024-06-27 23:52:59 -0400

    FM page - form submits to graph

--- src/pages/FinanceManagement.jsx
+++ src/pages/FinanceManagement.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PieChart from "../components/PieChart";
 
 import Button from "react-bootstrap/Button";
@@ -8,18 +8,75 @@ import Row from "react-bootstrap/Row";
 import InputGroup from "react-bootstrap/InputGroup";
 
 function FinanceManagement() {
-  //   var mortgage_rent = 0;
-  //   var utilities = 0;
-  //   var insurance = 0;
-  //   var loans = 0;
-  //   var transportation = 0;
-  //   var food = 0;
-  //   var other = 0;
-  var series = [54, 25, 41, 17, 15, 85, 19];
+  const [mortgageRent, setMortgageRent] = useState(0);
+  const [utilities, setUtilities] = useState(0);
+  const [insurance, setInsurance] = useState(0);
+  const [loans, setLoans] = useState(0);
+  const [transportation, setTransportation] = useState(0);
+  const [food, setFood] = useState(0);
+  const [other, setOther] = useState(0);
+  const [series, setSeries] = useState([
+    mortgageRent,
+    utilities,
+    insurance,
+    loans,
+    transportation,
+    food,
+    other,
+  ]);
+
+  useEffect(() => {
+    setSeries([
+      mortgageRent,
+      utilities,
+      insurance,
+      loans,
+      transportation,
+      food,
+      other,
+    ]);
+  }, [mortgageRent, utilities, insurance, loans, transportation, food, other]);
+
+
+  function HandleSubmit(e) {
+    let title = e.target[0].value;
+    let amount = parseInt(e.target[1].value);
+    let type = e.target[2].value;
+    console.log(`Title: ${title}`);
+    console.log(`Title type: ${typeof title}`);
+    console.log(amount);
+    console.log(`Amount type: ${typeof amount}`);
+    console.log(type);
+    console.log(`Type type: ${typeof type}`);
+
+    if (type === "mortgage_rent") {
+      setMortgageRent(mortgageRent + amount);
+    } else if (type === "utilities") {
+      setUtilities(utilities + amount);
+    } else if (type === "insurance") {
+      setInsurance(insurance + amount);
+    } else if (type === "loans") {
+      setLoans(loans + amount);
+    } else if (type === "transportation") {
+      setTransportation(transportation + amount);
+    } else if (type === "food") {
+      setFood(food + amount);
+    } else if (type === "other") {
+      setOther(other + amount);
+    } else {
+      alert("Please use a proper type.");
+    }
+
+    e.preventDefault();
+  }
 
   return (
     <div>
-      <Form style={{ width: "50%" }} className="mx-auto mt-5">
+      <Form
+        style={{ width: "50%" }}
+        className="mx-auto mt-5"
+        onSubmit={HandleSubmit}
+      >
         <Row className="mb-3">
           <Col>
             <Form.Group as={Col}>
@@ -32,18 +89,13 @@ function FinanceManagement() {
             <Form.Label>Amount Spent</Form.Label>
             <InputGroup className="mb-3">
               <InputGroup.Text>$</InputGroup.Text>
-              <Form.Control aria-label="Amount spent" />
-              {/* <Form.Control aria-label="Amount (to the nearest dollar)" /> */}
-              {/* <InputGroup.Text>.00</InputGroup.Text> */}
+              {/* <Form.Control aria-label="Amount spent" /> */}
+              <Form.Control aria-label="Amount (to the nearest dollar)" />
+              <InputGroup.Text>.00</InputGroup.Text>
             </InputGroup>
           </Col>
-
-          {/* <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={1} />
-          </Form.Group> */}
         </Row>
-
+        
         <Form.Select aria-label="Default select example" className="mb-3">
           <option>Type of Expense</option>
           <option value="mortgage_rent">Mortgage/Rent</option>
