581e0c7d0069794f9ebfc3d2bdec1c6e587ab29e
bschall20 <bschall2016@gmail.com>
2024-12-07 00:14:09 -0500

    Updating some env files + tab favicon. Adding firebase hosting

--- Frontend/src/db.js
+++ Frontend/src/db.js
@@ -1,16 +1,3 @@
-//import React from "react";
-// import postgres from 'postgres';
-
-// const sql = postgres({
-//   host                 : 'localhost',            // Postgres ip address[s] or domain name[s]
-//   port                 : 5432,          // Postgres server port[s]
-//   database             : 'Finance Tracker Website',            // Name of database to connect to
-//   username             : 'postgres',            // Username of database user
-//   password             : '',            // Password of database user
-// })
-
-// export default sql
-
 import pg from 'pg'
 const { Client } = pg
 const client = new Client()
